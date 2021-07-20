70959f67295198c4a5ca9d71fbd9f39f016d46c4
veljkoza <veljko.sekulic1998@gmail.com>
2021-07-21 00:13:28 +0200

    Makes graph accept real data and visualises it using recursive methods

---
+++ app.js
@@ -0,0 +1,116 @@
+const treeNodes = [{
+        nodeId: 1,
+        parentId: null,
+        label: 'ROOT'
+    },
+    {
+        nodeId: 2,
+        parentId: 1,
+        label: 'A'
+    },
+    {
+        nodeId: 3,
+        parentId: 1,
+        label: 'B'
+    },
+    {
+        nodeId: 4,
+        parentId: 2,
+        label: 'C'
+    },
+    {
+        nodeId: 5,
+        parentId: 2,
+        label: 'D'
+    },
+    {
+        nodeId: 6,
+        parentId: 3,
+        label: 'E'
+    },
+    {
+        nodeId: 7,
+        parentId: 3,
+        label: 'F'
+    },
+    {
+        nodeId: 8,
+        parentId: 6,
+        label: 'E'
+    },
+    {
+        nodeId: 9,
+        parentId: 6,
+        label: 'F'
+    },
+]
+
+const rootUl = document.getElementById('root-ul')
+let rootNode;
+
+const getRootNode = () => {
+    return treeNodes.filter(node => node.parentId === null)[0]
+}
+
+rootNode = getRootNode();
+
+const drawRoot = () => {
+    const rootLi = document.createElement('li')
+    const innerUl = document.createElement('ul')
+
+    let {
+        label
+    } = rootNode
+
+    rootLi.innerHTML = `<a href='#'><span>${label}</span></a>`
+    let childUl = rootLi.appendChild(innerUl)
+    rootUl.appendChild(rootLi)
+    return childUl
+}
+
+const drawNodesFrom = (node,ulElement) =>{
+    const childNodes = treeNodes.filter(x => x.parentId === node.nodeId)
+    
+    if(childNodes.length > 1){
+        let leftChild =  childNodes[0]
+        let rightChild = childNodes[1]
+
+        if(leftChild){
+            let innerUl = drawNode(leftChild,ulElement)
+            drawNodesFrom(leftChild,innerUl)
+        }
+
+        if(rightChild){
+            let innerUl = drawNode(rightChild,ulElement)
+            drawNodesFrom(rightChild,innerUl)
+        }
+    }
+}
+
+
+const drawNode = (node,ulElement) => {
+    const newLi = document.createElement('li')
+    const innerUl = document.createElement('ul')
+
+    let {
+        label
+    } = node
+
+    newLi.innerHTML = `<a href='#'><span>${label}</span></a>`
+    let childUl = newLi.appendChild(innerUl)
+    ulElement.appendChild(newLi)
+
+    return childUl
+
+
+}
+
+const drawTree = () => {
+    let rootUl = drawRoot()
+    console.log("🚀 ~ file: app.js ~ line 90 ~ drawTree ~ rootUl", rootUl)
+    drawNodesFrom(rootNode,rootUl)
+}
+drawTree()
+
+
+

64db2a059308faa69d2af70bd4cb514a1c5dd98f
veljkoza <veljko.sekulic1998@gmail.com>
2021-07-21 00:35:31 +0200

    Fixes error where child can only have 2 children or none

--- app.js
+++ app.js
@@ -1,116 +1,132 @@
-const treeNodes = [{
-        nodeId: 1,
-        parentId: null,
-        label: 'ROOT'
-    },
-    {
-        nodeId: 2,
-        parentId: 1,
-        label: 'A'
-    },
-    {
-        nodeId: 3,
-        parentId: 1,
-        label: 'B'
-    },
-    {
-        nodeId: 4,
-        parentId: 2,
-        label: 'C'
-    },
-    {
-        nodeId: 5,
-        parentId: 2,
-        label: 'D'
-    },
-    {
-        nodeId: 6,
-        parentId: 3,
-        label: 'E'
-    },
-    {
-        nodeId: 7,
-        parentId: 3,
-        label: 'F'
-    },
-    {
-        nodeId: 8,
-        parentId: 6,
-        label: 'E'
-    },
-    {
-        nodeId: 9,
-        parentId: 6,
-        label: 'F'
-    },
-]
+const treeNodes = [
+  {
+    nodeId: 1,
+    parentId: null,
+    label: "ROOT",
+  },
+  {
+    nodeId: 2,
+    parentId: 1,
+    label: "A",
+  },
+  {
+    nodeId: 3,
+    parentId: 1,
+    label: "B",
+  },
+  {
+    nodeId: 4,
+    parentId: 2,
+    label: "C",
+  },
+  {
+    nodeId: 5,
+    parentId: 2,
+    label: "D",
+  },
+  {
+    nodeId: 6,
+    parentId: 3,
+    label: "E",
+  },
+  {
+    nodeId: 7,
+    parentId: 3,
+    label: "F",
+  },
+  {
+    nodeId: 8,
+    parentId: 6,
+    label: "G",
+  },
+  {
+    nodeId: 9,
+    parentId: 6,
+    label: "H",
+  },
+  {
+    nodeId: 10,
+    parentId: 4,
+    label: "J",
+  },
+  {
+    nodeId: 11,
+    parentId: 4,
+    label: "J",
+  },
+  {
+    nodeId: 12,
+    parentId: 5,
+    label: "O",
+  },
+  {
+    nodeId: 13,
+    parentId: 5,
+    label: "O",
+  },
+  {
+    nodeId: 14,
+    parentId: 13,
+    label: "O",
+  },
+];
 
-const rootUl = document.getElementById('root-ul')
+const rootUl = document.getElementById("root-ul");
 let rootNode;
 
 const getRootNode = () => {
-    return treeNodes.filter(node => node.parentId === null)[0]
-}
+  return treeNodes.filter((node) => node.parentId === null)[0];
+};
 
 rootNode = getRootNode();
 
 const drawRoot = () => {
-    const rootLi = document.createElement('li')
-    const innerUl = document.createElement('ul')
+  const rootLi = document.createElement("li");
+  const innerUl = document.createElement("ul");
 
-    let {
-        label
-    } = rootNode
+  let { label } = rootNode;
 
-    rootLi.innerHTML = `<a href='#'><span>${label}</span></a>`
-    let childUl = rootLi.appendChild(innerUl)
-    rootUl.appendChild(rootLi)
-    return childUl
-}
+  rootLi.innerHTML = `<a href='#'><span>${label}</span></a>`;
+  let childUl = rootLi.appendChild(innerUl);
+  rootUl.appendChild(rootLi);
+  return childUl;
+};
 
-const drawNodesFrom = (node,ulElement) =>{
-    const childNodes = treeNodes.filter(x => x.parentId === node.nodeId)
-    
-    if(childNodes.length > 1){
-        let leftChild =  childNodes[0]
-        let rightChild = childNodes[1]
+const drawNodesFrom = (node, ulElement) => {
+  const childNodes = treeNodes.filter((x) => x.parentId === node.nodeId);
+  if (childNodes.length > 0) {
+    let leftChild = childNodes[0];
+    let rightChild = childNodes[1];
 
-        if(leftChild){
-            let innerUl = drawNode(leftChild,ulElement)
-            drawNodesFrom(leftChild,innerUl)
-        }
-
-        if(rightChild){
-            let innerUl = drawNode(rightChild,ulElement)
-            drawNodesFrom(rightChild,innerUl)
-        }
+    if (leftChild) {
+      let innerUl = drawNode(leftChild, ulElement);
+      drawNodesFrom(leftChild, innerUl);
+      console.log("end");
     }
-}
 
+    if (rightChild) {
+      let innerUl = drawNode(rightChild, ulElement);
+      drawNodesFrom(rightChild, innerUl);
+      console.log("end");
+    }
+  }
+};
 
-const drawNode = (node,ulElement) => {
-    const newLi = document.createElement('li')
-    const innerUl = document.createElement('ul')
+const drawNode = (node, ulElement) => {
+  const newLi = document.createElement("li");
+  const innerUl = document.createElement("ul");
 
-    let {
-        label
-    } = node
+  let { label } = node;
 
-    newLi.innerHTML = `<a href='#'><span>${label}</span></a>`
-    let childUl = newLi.appendChild(innerUl)
-    ulElement.appendChild(newLi)
+  newLi.innerHTML = `<a href='#'><span>${label}</span></a>`;
+  let childUl = newLi.appendChild(innerUl);
+  ulElement.appendChild(newLi);
 
-    return childUl
-
-
-}
+  return childUl;
+};
 
 const drawTree = () => {
-    let rootUl = drawRoot()
-    console.log("🚀 ~ file: app.js ~ line 90 ~ drawTree ~ rootUl", rootUl)
-    drawNodesFrom(rootNode,rootUl)
-}
-drawTree()
-
-
-
+  let rootUl = drawRoot();
+  drawNodesFrom(rootNode, rootUl);
+};
+drawTree();

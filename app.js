41d9baf453ac501e221f29d609d605977dfdd0d5
veljkoza <veljko.sekulic1998@gmail.com>
2021-07-22 18:17:36 +0200

    Adds drop off-zones

--- app.js
+++ app.js
@@ -66,10 +66,17 @@ const drawRoot = () => {
   const innerUl = document.createElement("ul");
 
   let {
-    label
+    label,
+    nodeId
   } = rootNode;
 
-  rootLi.innerHTML = `<a href='#'><span>${label}</span></a>`;
+  rootLi.innerHTML = `<button>
+                        <div class='drop-off' data-nodeId=${nodeId}>
+                          <p>-<p>
+                        </div>
+                        <span>${label}</span>
+                      </button>`;
+
   let childUl = rootLi.appendChild(innerUl);
   rootUl.appendChild(rootLi);
   return childUl;
@@ -97,24 +104,71 @@ const drawNodesFrom = (node, ulElement) => {
 
 
 
+const createNodeElement = node => {
+  let{nodeId,label} = node
+
+  let innerDiv = document.createElement('div')
+  let innerButton = document.createElement('button')
+  let nodeDropOffDiv = document.createElement('div')
+  let labelSpan = document.createElement('span')
+
+  innerButton.setAttribute('draggable', true)
+  innerButton.setAttribute('id', nodeId)
+  labelSpan.innerText = label
+  nodeDropOffDiv.innerHTML = `<p>-<p>`
+  nodeDropOffDiv.classList.add('drop-off')
+  nodeDropOffDiv.setAttribute('data-nodeId', nodeId)
+
+  innerButton.appendChild(labelSpan)
+  innerDiv.appendChild(innerButton)
+
+  let nodeHasChildren = isNodeParent(node);
+
+  // if (nodeHasChildren) {
+    innerButton.appendChild(nodeDropOffDiv)
+  // }
+
+  return innerDiv
+
+}
+
 const drawNode = (node, ulElement) => {
   const newLi = document.createElement("li");
   const innerUl = document.createElement("ul");
 
-  let {
-    label
-  } = node;
+  // let {
+  //   label,
+  //   nodeId
+  // } = node;
 
   let nodeHasChildren = isNodeParent(node);
-  let childUl
 
-  newLi.innerHTML = `<a href='#'><span>${label}</span></a>`;
+  // let innerDiv = document.createElement('div')
+  // let innerButton = document.createElement('button')
+  // let nodeDropOffDiv = document.createElement('div')
+  // let labelSpan = document.createElement('span')
 
-  // Dont append UL if node has no children
-  if (nodeHasChildren) {
+  // innerButton.setAttribute('draggable', true)
+  // innerButton.setAttribute('id', nodeId)
+  // labelSpan.innerText = label
+  // nodeDropOffDiv.innerHTML = `<p>-<p>`
+  // nodeDropOffDiv.classList.add('drop-off')
+  // nodeDropOffDiv.setAttribute('data-nodeId', nodeId)
+
+  // innerButton.appendChild(labelSpan)
+  // innerDiv.appendChild(innerButton)
+  let innerDiv = createNodeElement(node)
+  newLi.innerHTML = innerDiv.outerHTML
+
+
+  let childUl;
+  // Append UL only if node has children
+  // if (nodeHasChildren) {
+    // innerButton.appendChild(nodeDropOffDiv)
+    // newLi.innerHTML = innerDiv.outerHTML
+
     childUl = newLi.appendChild(innerUl);
-
-  }
+  // }
   ulElement.appendChild(newLi);
 
   return childUl;
@@ -124,4 +178,5 @@ const drawTree = () => {
   let rootUl = drawRoot();
   drawNodesFrom(rootNode, rootUl);
 };
+
 drawTree();
